c87f313603cb139b66b9ae155f21874728b16699
Frank2nd <frank.brink@darwin-it.nl>
2020-01-28 17:28:13 +0100

    change the file structure

--- oci/terraform/terraformenv.bat
+++
@@ -1,5 +0,0 @@
-setx TF_VAR_tenancy_ocid ocid1.tenancy.oc1..aaaaaaaa6jjldfvjyf7sjmmnq6gltqetamqeizj5fkuxazlmaxh7mbmgrkpa
-setx TF_VAR_compartment_ocid ocid1.compartment.oc1..aaaaaaaad2wnxlyd7j4pjx2i6i6osj5bkxkcbewqpzspqaymgjtujhqjpixq
-setx TF_VAR_user_ocid ocid1.user.oc1..aaaaaaaa4ofrc45on4jgzvxjs6vugt4m4obfuaxlj62wgmdcbbjhctneibiq
-setx TF_VAR_fingerprint 8c:dd:4f:4a:dd:7b:4b:25:40:96:39:fb:db:ce:67:0c
-setx TF_VAR_private_key_path C:/Users/frank.brink/.oci/oci_api_key.pem
